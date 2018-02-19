85aa50583f1c00e169e077cc32e6ccd4339625c5
Chau <ctch5@mail.umsl.edu>
2018-02-19 07:46:49 -0600

    Heroku prep

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+
+// const cropData = JSON.parse(fs.readFileSync('./csvjson.json','utf8'))
+// console.log(cropData)
+
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
+
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
